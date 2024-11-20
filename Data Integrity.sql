81d064de8a08316cea1b1d8702df55c884a5af37
kvig-93 <kvig.93@gmail.com>
2024-11-20 15:44:08 -0800

    Update Data Integrity.sql

--- Data Integrity.sql
+++ Data Integrity.sql
@@ -6,3 +6,7 @@ FROM
     Instrument i
 WHERE 
     i.Type = 'Combustion Analyzer';
+
+-- Based on the value we get at DaysSinceLastCalibration column and the standards set by the organization for calibration 
+-- For exmaple, say 15 or 30 days is the standard, we can compare the result to see when was the last time the insutrument  
+-- has been calibirated which would help in determining whether the combustion analyzer data can be trusted.
