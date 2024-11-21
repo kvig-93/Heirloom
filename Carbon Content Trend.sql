895a1c98ed79aa45e4128ddbc06d632420ff35c2
kvig-93 <kvig.93@gmail.com>
2024-11-21 12:58:40 -0800

    Update Carbon Content Trend.sql

--- Carbon Content Trend.sql
+++ Carbon Content Trend.sql
@@ -12,3 +12,7 @@ WHERE
     a.AnalysisType = 'CarbonContent'
 ORDER BY 
     p.StartTime;
+
+-- The above query will result the data in the form of table which will tell us how the CarbonContent has changed
+-- for each ProcessType like Calcining, Hydroxilating for each sample with the order of each process start time to give
+-- an idea of how the carboncontent trends over one process after other giving the effectiveness of each step in order.
